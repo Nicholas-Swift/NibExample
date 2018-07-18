be3ccb6fa47e371e548f9c6b6c5ac6df796ca7d9
Nick Swift <nswift@fb.com>
2018-07-18 12:17:03 -0700

    Initial Commit

---
+++ NibExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NibExample
+//
+//  Created by Nick Swift on 7/18/18.
+//  Copyright © 2018 Nick Swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NibExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NibExample
+//
+//  Created by Nick Swift on 7/18/18.
+//  Copyright © 2018 Nick Swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
